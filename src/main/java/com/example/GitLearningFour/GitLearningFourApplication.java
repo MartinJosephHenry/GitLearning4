28d2d85884b294fe250a1f1a5f7a9ab556580a26
Martin <martinhenryp@gmail.com>
2023-04-18 12:38:55 +0200

    "Dev first commit"

--- src/main/java/com/example/GitLearningFour/GitLearningFourApplication.java
+++ src/main/java/com/example/GitLearningFour/GitLearningFourApplication.java
@@ -8,7 +8,7 @@ public class GitLearningFourApplication {
 
 	public static void main(String[] args) {
 
-		System.out.println("in master");
+		System.out.println("in dev");
 		SpringApplication.run(GitLearningFourApplication.class, args);
 	}
 
