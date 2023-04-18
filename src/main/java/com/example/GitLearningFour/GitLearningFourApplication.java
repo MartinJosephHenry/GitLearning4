0ce75de3bcb3041a8c0740d40dfbba9a5d1042b4
Martin <martinhenryp@gmail.com>
2023-04-18 12:36:53 +0200

    "Master first commit"

--- src/main/java/com/example/GitLearningFour/GitLearningFourApplication.java
+++ src/main/java/com/example/GitLearningFour/GitLearningFourApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitLearningFourApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("in master");
 		SpringApplication.run(GitLearningFourApplication.class, args);
 	}
 
