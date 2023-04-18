21f63272c84cf591550c48125f7671a21c0df6cd
Martin <martinhenryp@gmail.com>
2023-04-18 12:10:02 +0200

    "First commit"

---
+++ src/main/java/com/example/GitLearningFour/GitLearningFourApplication.java
@@ -0,0 +1,13 @@
+package com.example.GitLearningFour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitLearningFourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitLearningFourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GitLearningFour/GitLearningFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GitLearningFour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitLearningFourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
