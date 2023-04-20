744cdd9c7a5859e28ae8d4ffd095e03d807cc918
Martin <martinhenryp@gmail.com>
2023-04-20 20:23:55 +0200

    Master branch

--- src/main/java/com/example/GitLearningFour/GitLearningFourApplication.java
+++ src/main/java/com/example/GitLearningFour/GitLearningFourApplication.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitLearningFourApplication {
 	public static void main(String[] args) {
 
-		System.out.println("issue1");
 		System.out.println("master2");
-		System.out.println("This a bug fix");
 		SpringApplication.run(GitLearningFourApplication.class, args);
 	}
 
